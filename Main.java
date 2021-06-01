d142fa7e02e17645c7e10aa881625306461fb6f4
JGaleano09 <84114203+JGaleano09@users.noreply.github.com>
2021-06-01 12:50:28 -0500

    Test File Java
    #java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int v_int = 32;
+        String v_salida = "Hello World!";
+        System.out.println (v_salida);
+        System.out.println (v_int);
+    }
+}
