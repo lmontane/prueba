1b1c179f5b138eda98f4eaeafc7414b1a4e7b9fb
CatherineLombana98 <85189422+CatherineLombana98@users.noreply.github.com>
2021-06-01 12:51:12 -0500

    TESTCL

---
+++ Inchcape.java
@@ -0,0 +1,11 @@
+public class Inchcape {
+    public static void main (String[]args) {
+        System.out.println("Hola Cathe");
+    }
+}
+
+public class Inchcape {
+    public static void main (String[]args) {
+        System.out.println("Environment");
+        }
+}
