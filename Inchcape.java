3b8baf9b746f3bb7b778c6cead5fd2ece74d5aa7
Moni202 <61317650+Moni202@users.noreply.github.com>
2021-06-01 14:22:13 -0500

    Update Inchcape.java
    Nuevo java

--- Inchcape.java
+++ Inchcape.java
@@ -9,3 +9,15 @@ public class Inchcape {
         System.out.println("Environment");
         }
 }
+
+
+public class Example {
+    public static void main(String[] args) {
+        String thanks = "Thank you,";
+        String name = "miguel!";
+        String order = "Your order number is: #";
+        int previousOrder = 715;
+        System.out.println(thanks + " " + name.toUpperCase());
+        System.out.println(order + (previousOrder + 1));
+    }
+}
