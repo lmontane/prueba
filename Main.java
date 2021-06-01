8eb7d559ba754c016a78cd424697e019e1ff6b9f
JGaleano09 <84114203+JGaleano09@users.noreply.github.com>
2021-06-01 12:54:12 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
 public class Main {
     public static void main(String[] args){
-        int v_int = 32;
-        String v_salida = "Hello World!";
-        System.out.println (v_salida);
-        System.out.println (v_int);
+        int v_int = 14;
+        char v_char = 'Y';
+        String v_string = "Hello!";
+        Double v_double = 2.5;
+        float v_float = 2.5f;
+        short v_short = 307;
+        byte v_byte = -1;
+        long v_long = 438000000L;
+        boolean v_boolean = false;
+
+        System.out.println(v_int);
+        System.out.println(v_char);
+        System.out.println(v_string);
+        System.out.println(v_double);
+        System.out.println(v_float);
+        System.out.println(v_short);
+        System.out.println(v_byte);
+        System.out.println(v_long);
+        System.out.println(v_boolean);
+
+        String v_thanks = "Thank you!";
+        String v_name = "miguel!";
+        String v_order = "Your order number is #";
+        int v_number = 715;
+
+        System.out.println(v_thanks + " " + v_name.toUpperCase());
+        System.out.println(v_order + (v_number + 1));
     }
 }
